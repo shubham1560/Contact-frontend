ed48bbd672887a8020c5f8a81dd6687b5c6991d0
Bhubanesh Pinani <shubhamsinha2050@gmail.com>
2020-09-25 00:22:24 +0530

    Added cdk library

--- src/app/shared/material.module.ts
+++ src/app/shared/material.module.ts
@@ -1,17 +1,19 @@
 import { NgModule } from '@angular/core';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatFormFieldModule} from '@angular/material/form-field';
-
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [],
   imports: [
     MatToolbarModule,
     MatFormFieldModule,
+    MatButtonModule
   ],
   exports:[
     MatToolbarModule,
-    MatFormFieldModule
+    MatFormFieldModule,
+    MatButtonModule
 ],
 })
 export class MaterialModule { }
