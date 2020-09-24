4bd8f437333d797a07319b971309066ed1881a5a
Bhubanesh Pinani <shubhamsinha2050@gmail.com>
2020-09-25 00:02:10 +0530

    Something peculiar

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -4,13 +4,17 @@ import { CommonModule } from '@angular/common';
 import { AuthRoutingModule } from './auth-routing.module';
 import { SignInComponent } from './sign-in/sign-in.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
+import { MaterialModule } from '../shared/material.module';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 
 @NgModule({
   declarations: [SignInComponent, SignUpComponent],
   imports: [
     CommonModule,
-    AuthRoutingModule
+    AuthRoutingModule,
+    MaterialModule,
+    MatFormFieldModule,
   ]
 })
 export class AuthModule { }
--- src/app/shared/material.module.ts
+++ src/app/shared/material.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatFormFieldModule} from '@angular/material/form-field';
+
 
 @NgModule({
   declarations: [],
   imports: [
     MatToolbarModule,
+    MatFormFieldModule,
   ],
   exports:[
-    MatToolbarModule
+    MatToolbarModule,
+    MatFormFieldModule
 ],
 })
 export class MaterialModule { }
