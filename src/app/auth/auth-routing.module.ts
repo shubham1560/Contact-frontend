e6926731cce425a938dd0bf12a130b2d31ea1e63
Bhubanesh Pinani <shubhamsinha2050@gmail.com>
2020-09-25 12:17:33 +0530

    Added registration form

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MaterialModule } from '../shared/material.module'
 import { SignInComponent } from './sign-in/sign-in.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
 const routes: Routes = [
   {
     path: "",
     component: SignInComponent
+  },
+  {
+    path: "register",
+    component: SignUpComponent
   }
 ];
 
--- src/app/auth/sign-up/sign-up.component.ts
+++ src/app/auth/sign-up/sign-up.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, Validators, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up',
@@ -7,9 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignUpComponent implements OnInit {
 
-  constructor() { }
+  registrationForm: FormGroup;
+  hide: boolean = true;
+
+  constructor(
+    private fb: FormBuilder
+  ) { }
 
   ngOnInit(): void {
+    this.registrationForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8)]],
+      // passwordGroup: this.fb.group({
+      //   password: ['', [Validators.required]],
+      //   confirm_password: ['', [Validators.required]]
+      // }, {validators: passwordMatch}),
+      full_name: ['', [Validators.required,]],
+      company:['', [Validators.required,]]
+    },)
   }
 
+  register(){
+    console.log("register")
+  }
 }
