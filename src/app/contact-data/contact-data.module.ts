17ade96c80a544c478580149b73266f1d9635a80
Bhubanesh Pinani <shubhamsinha2050@gmail.com>
2020-09-26 10:20:22 +0530

    Added pages module

---
+++ src/app/contact-data/contact-data.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ContactDataRoutingModule } from './contact-data-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ContactDataRoutingModule
+  ]
+})
+export class ContactDataModule { }
---
+++ src/app/product-docs/product-docs.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ProductDocsRoutingModule } from './product-docs-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ProductDocsRoutingModule
+  ]
+})
+export class ProductDocsModule { }
