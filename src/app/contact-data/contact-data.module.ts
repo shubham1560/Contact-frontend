c82c0113afb8cacf9b0d94a3f3eadeeab3a00d6b
Bhubanesh Pinani <shubhamsinha2050@gmail.com>
2020-09-26 11:20:32 +0530

    Added data table

--- src/app/contact-data/contact-data.module.ts
+++ src/app/contact-data/contact-data.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ContactDataRoutingModule } from './contact-data-routing.module';
+import { DataTableComponent } from './data-table/data-table.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [DataTableComponent],
   imports: [
     CommonModule,
     ContactDataRoutingModule
